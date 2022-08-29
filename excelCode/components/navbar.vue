27f013339c5bde3ac9b5ec2ade8d8e59eb6ea50d
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2022-08-29 19:28:02 -0400

    8/29/22 7:29PM
    touched up the nav bar and redid hexagon on home page as a unicode symbol

--- excelCode/components/navbar.vue
+++ excelCode/components/navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <section id="navbar">
     <div class="nav-container">
-        <NuxtLink class="route" to="/">Self-Guided</NuxtLink>
-        <NuxtLink class="route" to="/">Education Home</NuxtLink>
-        <div id="login">
-            <NuxtLink class="login" to="/">Login | Sign Up</NuxtLink>
-        </div>
+      <NuxtLink class="route" to="/">Self-Guided</NuxtLink>
+      <NuxtLink class="route" to="/">Education Home</NuxtLink>
+      <div id="login">
+        <NuxtLink class="login" to="/">Login | Sign Up</NuxtLink>
+      </div>
     </div>
   </section>
 </template>
@@ -15,52 +15,54 @@ export default {};
 </script>
 
 <style scoped>
-#navbar{
-    display: flex;
-    flex-direction: row;
-    /* left: 0;
+#navbar {
+  height: 6vh;
+  display: flex;
+  flex-direction: row;
+  /* left: 0;
     top: 0; */
-    position: fixed;
-    width: 88vw;
-    align-items: center;
-    /* justify-content: space-around; */
-    border-bottom: 2px solid black;
-    /* padding: 1rem 4rem; */
-    margin-left: 3.5rem;
-    /* margin-right: 3rem; */
-    padding: 0.8rem;
-    /* background: red; */
+  position: fixed;
+  width: 88vw;
+  align-items: center;
+  /* justify-content: space-around; */
+  border-bottom: 2px solid black;
+  /* padding: 1rem 4rem; */
+  margin-left: 3.5rem;
+  /* margin-right: 3rem; */
+  padding: 0.8rem;
+  /* background: red; */
 }
 
-.nav-container{
-    transform: translateX(150%);
-    display: flex;
-    width: 40%;
-    justify-content: space-around;
-    font-family: Arial, Helvetica, sans-serif;
+.nav-container {
+  transform: translateX(150%);
+  display: flex;
+  width: 40%;
+  justify-content: space-around;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
-.route{
-    text-decoration: none;
-    font-size: 0.75rem;
-    color: black;
-    border: 1px solid #EFEFF0;
-    border-radius: 12px;
-    padding: 6.5px 15px;
-    box-shadow: 0 5px 5px rgba(0,0,0,0.20);
-
+.route {
+  text-decoration: none;
+  font-size: 0.85rem;
+  color: black;
+  border: 1px solid #efeff0;
+  border-radius: 12px;
+  padding: 6.5px 15px;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
+  width: 23%;
+  text-align: center;
 }
 
-#login{
-    margin-top: 0.25rem;
+#login {
+  margin-top: 0.25rem;
 }
 
-.login{
-    font-size: 0.9rem;
-    color: white;
-    background-color: #0F2337;
-    text-decoration: none;
-    padding: 15px 15px;
-    border-radius: 13px;
+.login {
+  font-size: 0.9rem;
+  color: white;
+  background-color: #0f2337;
+  text-decoration: none;
+  padding: 15px 15px;
+  border-radius: 13px;
 }
 </style>
--- excelCode/pages/index.vue
+++ excelCode/pages/index.vue
@@ -1,53 +1,59 @@
 <template>
-    <section class="home">
-        <div class="row-1">
-            <div class="hexagon">
-                <div class="sub-header"></div>
-                <div class="explore"></div>
-            </div>
-            <!-- <div class="hexagon">
+  <section class="home">
+    <div class="row-1">
+      <!-- <div class="hexagon">
+        <div class="sub-header"></div>
+        <div class="explore"></div>
+      </div> -->
+      <span class="hex">&#x2B22;</span>
+      <!-- <div class="hexagon">
                 <div class="sub-header"></div>
                 <div class="explore"></div>
             </div> -->
-        </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-.home{
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* background-color: #fbb12f; */
+.home {
+  height: 94vh;
+  width: 100vw;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color: #fbb12f; */
 }
 
-.hexagon{
-    height: 0;
-    width: 150px;
-    border-top: 150px solid #8FAC86;
-    border-right: 100px solid transparent;
-    border-left: 100px solid transparent;
-    position: relative;
-    transform: rotate(-90deg) scale(0.7);
+/* .hexagon {
+  height: 0;
+  width: 150px;
+  border-top: 150px solid #8fac86;
+  border-right: 100px solid transparent;
+  border-left: 100px solid transparent;
+  position: relative;
+  transform: rotate(-90deg) scale(0.7);
 }
 
-.hexagon:before{
-    position: absolute;
-    content: '';
-    height: 0;
-    width: 150px;
-    border-bottom: 150px solid #8FAC86;
-    border-right: 100px solid transparent;
-    border-left: 100px solid transparent;
-    bottom: 150px;
-    left: -100px;
+.hexagon:before {
+  position: absolute;
+  content: "";
+  height: 0;
+  width: 150px;
+  border-bottom: 150px solid #8fac86;
+  border-right: 100px solid transparent;
+  border-left: 100px solid transparent;
+  bottom: 150px;
+  left: -100px;
+} */
+
+.hex {
+  color: #8fac86;
+  font-size: 259.8076px;
+  text-shadow: 10px 15px 25px #444444;
 }
 </style>
