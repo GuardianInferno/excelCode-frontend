7e2d6495dacd474cbaa70ecc4f6407727724ab9b
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-08-29 16:28:10 -0400

    set upp navbar

---
+++ excelCode/components/navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <section id="navbar">
+    <div class="nav-container">
+        <NuxtLink class="route" to="/">Self-Guided</NuxtLink>
+        <NuxtLink class="route" to="/">Education Home</NuxtLink>
+        <div id="login">
+            <NuxtLink class="login" to="/">Login | Sign Up</NuxtLink>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#navbar{
+    display: flex;
+    flex-direction: row;
+    /* left: 0;
+    top: 0; */
+    position: fixed;
+    width: 88vw;
+    align-items: center;
+    /* justify-content: space-around; */
+    border-bottom: 2px solid black;
+    /* padding: 1rem 4rem; */
+    margin-left: 3.5rem;
+    /* margin-right: 3rem; */
+    padding: 0.8rem;
+    /* background: red; */
+}
+
+.nav-container{
+    transform: translateX(150%);
+    display: flex;
+    width: 40%;
+    justify-content: space-around;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.route{
+    text-decoration: none;
+    font-size: 0.75rem;
+    color: black;
+    border: 1px solid #EFEFF0;
+    border-radius: 12px;
+    padding: 6.5px 15px;
+    box-shadow: 0 5px 5px rgba(0,0,0,0.20);
+
+}
+
+#login{
+    margin-top: 0.25rem;
+}
+
+.login{
+    font-size: 0.9rem;
+    color: white;
+    background-color: #0F2337;
+    text-decoration: none;
+    padding: 15px 15px;
+    border-radius: 13px;
+}
+</style>
--- excelCode/nuxt.config.ts
+++ excelCode/nuxt.config.ts
@@ -2,5 +2,40 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app:{
+        head: {
+          title: "ExcelCode",
+          htmlAttrs: {
+            lang: 'en',
+          },
+          meta: [
+            // <meta name="viewport" content="width=device-width, initial-scale=1">
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+          ],
+          // script: [
 
+          //   ],
+          link: [
+ 
+          ],
+          components: true,
+          gsap: {
+            extraPlugins: {
+              scrollTo: true,
+              scrollTrigger: true,
+            },
+          },
+          
+          // // please note that this is an area that is likely to change
+          // style: [
+
+          // ]
+        },
+        
+      
+      
+       }
 })
