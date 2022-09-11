c848e134c8c9c1a8a738c9364ed5d92669c0d66b
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2022-09-11 01:25:20 -0400

    finished self guided home and login/signup

---
+++ excelCode/pages/signup.vue
@@ -0,0 +1,140 @@
+<template>
+ <div class="cont">
+  <h1 class="header">Welcome</h1>
+  <form
+   @submit.prevent="onSubmit"
+   action=""
+   method="post"
+   class="form-cont"
+  >
+   <input
+    class="input-form"
+    type="text"
+    placeholder="Enter Email Address"
+    name="email"
+   />
+   <input
+    class="input-form"
+    type="text"
+    placeholder="Enter Username"
+    name="username"
+    required
+   />
+   <input
+    :type="passwordFieldType"
+    v-model="passwordOne"
+    class="input-form"
+    placeholder="Enter Password"
+    name="password"
+    required
+   />
+   <div type="password" @click="switchVisibility">show / hide</div>
+   <input
+    :type="passwordFieldType2"
+    v-model="passwordTwo"
+    class="input-form"
+    placeholder="Confirm Password"
+    name="Confirm-Password"
+    required
+   />
+   <div type="password" @click="switchVisibility2">show / hide</div>
+   <select id="gradeForm" name="grade-level" required>
+    <option value="5">Grade 5</option>
+    <option value="6">Grade 6</option>
+    <option value="7">Grade 7</option>
+    <option value="8">Grade 8</option>
+    <option value="9">Grade 9</option>
+    <option value="10">Grade 10</option>
+    <option value="11">Grade 11</option>
+    <option value="12">Grade 12</option>
+   </select>
+   <button class="submit-btn" type="submit">Signup</button>
+  </form>
+ </div>
+</template>
+
+<script>
+export default {
+ data() {
+  return {
+   passwordOne: "",
+   passwordTwo: "",
+   passwordFieldType: "password",
+   passwordFieldType2: "password",
+  };
+ },
+ methods: {
+  switchVisibility() {
+   this.passwordFieldType =
+    this.passwordFieldType === "password" ? "text" : "password";
+  },
+  switchVisibility2() {
+   this.passwordFieldType2 =
+    this.passwordFieldType2 === "password" ? "text" : "password";
+  },
+ },
+};
+</script>
+
+<style scoped>
+.header {
+ font-size: 5rem;
+}
+.cont {
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ height: 95vh;
+ width: 100vw;
+}
+
+.submit-btn {
+ margin: 2rem;
+ font-size: 2rem;
+ height: 8%;
+ width: 40%;
+ border-radius: 2rem;
+ color: white;
+ background-color: black;
+ text-decoration: none;
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+}
+
+.form-cont {
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ height: 70%;
+ width: 70%;
+}
+
+#gradeForm {
+ width: 20%;
+ font-size: 1.5rem;
+ height: 10%;
+ padding-left: 1.3rem;
+ border-radius: 2rem;
+ margin: 2rem;
+ color: black;
+ background-color: #e7e7ea;
+ border: 1px solid #ccc;
+ box-sizing: border-box;
+}
+.input-form {
+ width: 50%;
+ font-size: 1.5rem;
+ height: 10%;
+ padding-left: 1.3rem;
+ border-radius: 2rem;
+ margin: 2rem;
+ color: black;
+ background-color: #e7e7ea;
+ border: 1px solid #ccc;
+ box-sizing: border-box;
+}
+</style>
