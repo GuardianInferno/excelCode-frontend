ac992c3027848d24ed29c2773ee5da38ef47eae0
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2022-09-18 23:41:20 -0400

    Vaceeditor

--- excelCode/components/codeEditor.vue
+++ excelCode/components/codeEditor.vue
@@ -1,23 +1,40 @@
 <template>
  <div class="cont">
-  <div class="code-cont">
-   <section class="inner-code-cont">
-    <textarea v-model="contentHTML" placeholder="html"></textarea>
-   </section>
-   <section class="inner-code-cont">
-    <textarea v-model="contentCSS" placeholder="CSS"></textarea>
-   </section>
+  <div class="code-editor-cont">
+   <v-ace-editor
+    v-model:value="contentHTML"
+    @init="editorInit"
+    lang="html"
+    theme="chrome"
+    style="height: 100%; width: 45%"
+    :options="{
+     wrap: true,
+    }"
+   />
+   <v-ace-editor
+    v-model:value="contentCSS"
+    @init="editorInit"
+    lang="css"
+    theme="chrome"
+    style="height: 100%; width: 45%"
+    :options="{
+     wrap: true,
+    }"
+   />
   </div>
-  <iframe id="iframe" srcdoc="codepen clone"></iframe>
+  <iframe id="iframe"></iframe>
  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-const contentCSS = ref("");
-const contentHTML = ref("");
+import { VAceEditor } from "vue3-ace-editor";
+
+const contentHTML = ref();
+const contentCSS = ref();
+
 onMounted(() => {
- // Replaces CTRL-S to run editor
+ // cntrl s for run if you dont want to do aouto fill
  document.addEventListener("keydown", (e) => {
   if (e.ctrlKey && e.key === "s") {
    try {
@@ -67,21 +84,12 @@ onBeforeUpdate(() => {
  width: 100vw;
  display: flex;
  flex-direction: column;
+ justify-content: space-evenly;
 }
-
-.code-cont {
+.code-editor-cont {
  display: flex;
  width: 100%;
  height: 50%;
-}
-.inner-code-cont {
- width: 50%;
- height: 100%;
-}
-
-textarea {
- height: 100%;
- width: 100%;
- word-wrap: wrap;
+ justify-content: space-evenly;
 }
 </style>
