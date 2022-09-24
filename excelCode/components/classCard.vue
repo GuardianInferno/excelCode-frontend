0d8c0b999f1836aa4560a758547f7f2e092f22b4
Hashir <hashirabbasi112@gmail.com>
2022-09-24 19:54:44 -0400

    Update classCard.vue

--- excelCode/components/classCard.vue
+++ excelCode/components/classCard.vue
@@ -20,6 +20,12 @@
         height:25rem;
         width:25rem;
         border-radius:40px;
+        box-shadow:  0 7px 7px rgba(0, 0, 0, 0.2);
+        transition: 0.3s ease-in-out;
+        &:hover{
+            background-color:#192c3f;
+            cursor: pointer;
+        }
         .content{
             .title{
                 font-size: 2.2rem;
