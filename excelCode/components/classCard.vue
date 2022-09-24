104d913e75756e9295929c98a0ead2d43c77ef8a
Hashir <hashirabbasi112@gmail.com>
2022-09-24 19:50:29 -0400

    class card is component

---
+++ excelCode/components/classCard.vue
@@ -0,0 +1,32 @@
+<template>
+        <div class="card">
+            <div class="content">
+                <div class="title">
+                    Period 3
+                </div>
+            </div>
+        </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped lang="scss">
+    .card{
+        background-color:#0f2337;
+        color:white;
+        height:25rem;
+        width:25rem;
+        border-radius:40px;
+        .content{
+            .title{
+                font-size: 2.2rem;
+                padding: 2rem 0rem 0rem 3rem;
+            }
+        }
+    }
+
+
+</style>
--- excelCode/components/teacherProfile.vue
+++ excelCode/components/teacherProfile.vue
@@ -1,17 +1,16 @@
 <template>
     <div class="container">
-        <div class="card">
-            <div class="content">
-                <div class="title">
-                    Period 3
-                </div>
-            </div>
+        <div class="cards">
+            <ClassCard />
+            <ClassCard />
+            <ClassCard />
         </div>
+
     </div>
 </template>
 
 <script setup lang="ts">
-
+import classCard from 'classCard.vue'
 
 </script>
 
@@ -21,20 +20,11 @@
     width: 100vw;
     display: flex;
     padding: 7rem 10rem 0 10rem;
-    .card{
-        background-color:#0f2337;
-        color:white;
-        height:25rem;
-        width:25rem;
-        border-radius:40px;
-        .content{
-            .title{
-                font-size: 2.2rem;
-                padding: 2rem 0rem 0rem 3rem;
-            }
-        }
+    .cards{
+        display: flex;
+        width: 100%;
+        justify-content: space-evenly;
     }
-
 }
 
 </style>
